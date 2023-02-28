1b668df1ed58d9b6bb1622d732edf24467a84860
unknown <garcia.arencon@bootcamp-upgrade.com>
2023-02-28 21:18:27 +0100

    feat

--- src/api/controllers/videogames.controllers.js
+++ src/api/controllers/videogames.controllers.js
@@ -1,35 +1,73 @@
 const Videogame = require("../models/videogames.models");
 
 const getAllVideogames = async (req, res) => {
- 
-   
-  
-    try { 
-        // console.log(req.query);
-        const { name } = req.query;
-        // console.log(name);
-         const allVideogames = await Videogame.find();
-         let filteredVideogameList = [...allVideogames]
 
-         if (name) {
-            filteredVideogameList = filteredVideogameList.filter((videogame)=>videogame.title.toLowerCase().match(name.toLocaleLowerCase()));
-         }
+  try {
+    const allVideogames = await Videogame.find();
 
-         if (!filteredVideogameList) {
-             return res.json({
-                status: 404,
-                message: "videogame not found"
-             })
-         }
-         return res.json({
-            videogames: filteredVideogameList
-         })
+    if (!allVideogames) {
+      return res.json({
+        status: 404,
+        message: "videogame not found",
+      });
+    }
+    return res.json({
+      videogames: allVideogames,
+    });
+  } catch (error) {
+    return res.json({ message: "error", error: error });
+  }
+};
 
+const getFilteredVideogames = async (req, res) => {
+  try {
+    // console.log(req.query);
+    const { name } = req.query;
+    // console.log(name);
+    const allVideogames = await Videogame.find();
+    let filteredVideogameList = [...allVideogames];
+
+    if (name) {
+      filteredVideogameList = filteredVideogameList.filter((videogame) =>
+        videogame.title.toLowerCase().match(name.toLocaleLowerCase())
+      );
+    }
+
+    if (!filteredVideogameList) {
+      return res.json({
+        status: 404,
+        message: "videogame not found",
+      });
+    }
+    return res.json({
+      videogames: filteredVideogameList,
+    });
+  } catch (error) {
+    return res.json({ message: "error", error: error });
+  }
+};
+
+const getVideogameID = async (req, res) => {
+
+    try {
+        // const id = req.params.id
+        const { id } = req.params;
+
+        const videogameId = await Videogame.findById(id)
+         
+        if (!videogameId) {
+
+            return res.status(404).send("Videogame not found");
+            
+        }
+        return res.json({
+            videogame: videogameId
+        })
 
     } catch (error) {
-        return res.json({message: "error", error: error})
+        return res.json({message: "Error", error:error})
+
     }
 }
 
-
-module.exports = { getAllVideogames };
+module.exports = { getAllVideogames, getFilteredVideogames, getVideogameID };
--- src/api/routes/videogames.routes.js
+++ src/api/routes/videogames.routes.js
@@ -1,10 +1,20 @@
 const router = require("express").Router();
 
 const {
-    getAllVideogames
+    getAllVideogames,
+    getFilteredVideogames,getVideogameID
 } = require("../controllers/videogames.controllers");
 
 router.get("/", getAllVideogames);
+router.get("/filter", getFilteredVideogames);
+router.get("/search/:id", getVideogameID);
+
+
+
+
+
+
+
 
 
 module.exports = router;
